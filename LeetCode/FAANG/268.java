2864beb13cde51d5bc663f03d3eb735ce07c230c
gagannagpal131 <gagannagpal131@gmail.com>
2020-10-29 18:53:06 -0400

    268. Missing Number

---
+++ LeetCode/FAANG/268.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+
+        Arrays.sort(nums);
+
+        for(int i = 0; i < nums.length; i++){
+
+            if(nums[i] > i)
+                return i;
+        }
+
+        return nums.length;
+    }
+}
